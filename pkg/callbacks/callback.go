3c70c19461fc34906a6e746a7f88bb88dbb000f5
Leonardo F <leonardofe@pm.me>
2020-07-22 19:30:24 -0300

    fix: weird spaces/tabs

--- pkg/callbacks/callback.go
+++ pkg/callbacks/callback.go
@@ -1,47 +1,47 @@
 package callbacks
 
 import (
-	"github.com/gotk3/gotk3/cairo"
-	"github.com/gotk3/gotk3/gdk"
-	"github.com/gotk3/gotk3/gtk"
-	"github.com/valeyard1/pineapple-pen/pkg/utils"
-	"log"
+    "github.com/gotk3/gotk3/cairo"
+    "github.com/gotk3/gotk3/gdk"
+    "github.com/gotk3/gotk3/gtk"
+    "github.com/valeyard1/pineapple-pen/pkg/utils"
+    "log"
 )
 
 const (
-	Title  = "Pineapple Pen"
+    Title  = "Pineapple Pen"
 )
 
 type pineapplePenData struct{
-	surface  *cairo.Surface            // context surface
+    surface  *cairo.Surface            // context surface
     cr       *cairo.Context            // main cairo context
-	debug    bool                      // true if you want debug messages
-	width    int                       // width of window
-	height   int                       // height of window
-	x        float64                   // idk yet
-	y        float64                   // idk yet
+    debug    bool                      // true if you want debug messages
+    width    int                       // width of window
+    height   int                       // height of window
+    x        float64                   // idk yet
+    y        float64                   // idk yet
 }
 
 var penData = pineapplePenData{
     debug: true,
-	width: 640,
-	height: 480,
-	x: 0.0,
-	y: 0.0,
+    width: 640,
+    height: 480,
+    x: 0.0,
+    y: 0.0,
 }
 
 // Startup signal
 func Startup() bool {
-	log.Println("Application started")
+    log.Println("Application started")
 
-	return true
+    return true
 }
 
 // Shutdown signal
 func Shutdown() bool {
-	log.Println("Exited")
+    log.Println("Exited")
 
-	return true
+    return true
 }
 
 
@@ -49,32 +49,32 @@ func Shutdown() bool {
 
 func Activate(application *gtk.Application) bool {
 
-	Width, Height := penData.width, penData.height
+    Width, Height := penData.width, penData.height
 
-	window, err := gtk.ApplicationWindowNew(application)
-	utils.ErrorCheck("Failed to create window: ", err)
+    window, err := gtk.ApplicationWindowNew(application)
+    utils.ErrorCheck("Failed to create window: ", err)
 
-	// Set window configuration
-	window.SetTitle(Title)
-	window.SetPosition(gtk.WIN_POS_CENTER)
-	window.SetDefaultSize(Width, Height)
+    // Set window configuration
+    window.SetTitle(Title)
+    window.SetPosition(gtk.WIN_POS_CENTER)
+    window.SetDefaultSize(Width, Height)
 
     penData.surface = cairo.CreateImageSurface(cairo.FORMAT_ARGB32, Width, Height)
     penData.cr = cairo.Create(penData.surface)
 
-	// Drawing Area
-	da, _ := gtk.DrawingAreaNew()
+    // Drawing Area
+    da, _ := gtk.DrawingAreaNew()
 
-	// Signal Handlers
-	da.Connect("draw", draw)
+    // Signal Handlers
+    da.Connect("draw", draw)
     window.Connect("motion_notify_event", motionNotifyEvent)
     window.Connect("button_press_event", buttonPressEvent)
 
-	window.Add(da)
-	window.ShowAll()
+    window.Add(da)
+    window.ShowAll()
 
 
-	return true
+    return true
 }
 
 //
@@ -91,11 +91,11 @@ func draw(_ *gtk.DrawingArea, cr *cairo.Context) bool {
     cr.SetOperator(cairo.OPERATOR_SOURCE)
     cr.Paint()
 
-	return true
+    return true
 }
 
 func motionNotifyEvent(app *gtk.ApplicationWindow, ev *gdk.Event) bool {
-	x, y := gdk.EventMotionNewFromEvent(ev).MotionVal()
+    x, y := gdk.EventMotionNewFromEvent(ev).MotionVal()
 
     if penData.debug {
         log.Printf("DEBUG: Got motion event on coord: (%f, %f)", x, y)
@@ -104,11 +104,11 @@ func motionNotifyEvent(app *gtk.ApplicationWindow, ev *gdk.Event) bool {
     drawLine(x, y)
     app.QueueDraw()
 
-	return true
+    return true
 }
 
 func buttonPressEvent(app *gtk.ApplicationWindow, ev *gdk.Event) bool {
-	x, y := gdk.EventMotionNewFromEvent(ev).MotionVal()
+    x, y := gdk.EventMotionNewFromEvent(ev).MotionVal()
 
     if penData.debug {
         log.Printf("DEBUG: Got button Press event on coord: (%f, %f)", x, y)
