396e73d5b29de33621241c9cec3b5f209be542dd
Leonardo F <leonardofe@pm.me>
2020-08-29 21:24:13 -0300

    feat: implement a minimum drawing area

--- src/callbacks.rs
+++ src/callbacks.rs
@@ -1,15 +1,29 @@
+use log::debug;
 use gtk::prelude::*;
-use log::*;
+
+struct PineappleData {
+    width: i32,
+    height: i32,
+}
 
 // Default signals
 pub fn on_activate(application: &gtk::Application) {
-    // … create a new window …
+    let pen_data = PineappleData {
+        width: 640,
+        height: 480,
+    };
+
     let window = gtk::ApplicationWindow::new(application);
-    // … with a button in it …
-    let button = gtk::Button::with_label("Hello World!");
-    // … which closes the window when clicked
-    button.connect_clicked(glib::clone!(@weak window => move |_| window.close()));
-    window.add(&button);
+
+    // Set window configuration
+    window.set_title("Pineapple Pen");
+    window.set_default_size(pen_data.width, pen_data.height);
+    window.set_position(gtk::WindowPosition::Center);
+
+    let drawing_area = gtk::DrawingArea::new();
+    drawing_area.connect_draw(|w,c|draw(w, c));
+
+    window.add(&drawing_area);
     window.show_all();
 }
 
@@ -21,5 +35,11 @@ pub fn on_shutdown(_: &gtk::Application) {
     debug!("Application exited");
 }
 
-
 // Custom callbacks
+fn draw(_: &gtk::DrawingArea, c: &cairo::Context) -> glib::signal::Inhibit {
+    debug!("Got draw event");
+    c.set_source_rgb(25.0, 255.0, 129.0);
+    c.rectangle(1.0, 1.0, 100.0, 200.0);
+    c.fill();
+    gtk::Inhibit(false)
+}
