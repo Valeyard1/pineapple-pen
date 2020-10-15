697720a3e667502e8cece646ece692eea4ec33f4
Leonardo F <leonardofe@pm.me>
2020-10-15 18:30:14 -0300

    just save it

--- src/callbacks.rs
+++ src/callbacks.rs
@@ -1,25 +1,23 @@
 use log::debug;
 use gtk::prelude::*;
 
-struct PineappleData {
-    width:  i32,            // monitor width
-    height: i32,            // monitor height
+// struct PineappleData {
+    // // width:  i32,            // monitor width
+    // // height: i32,            // monitor height
     // cr:     cairo::Context, // cairo context should be 
-}
+// }
 
 // Default signals
 pub fn on_activate(application: &gtk::Application) {
-    let pen_data = PineappleData {
-        width: 640,
-        height: 480,
-    };
-
     let window = gtk::ApplicationWindow::new(application);
     let drawing_area = gtk::DrawingArea::new();
 
+    let width = 640;
+    let height = 480;
+
     // Set window configuration
     window.set_title("Pineapple Pen");
-    window.set_default_size(pen_data.width, pen_data.height);
+    window.set_default_size(width, height);
     window.set_position(gtk::WindowPosition::Center);
 
     // Connect signals
@@ -51,18 +49,32 @@ fn draw(_: &gtk::DrawingArea, c: &cairo::Context) -> glib::signal::Inhibit {
     gtk::Inhibit(false)
 }
 
+// Triggered when I move the mouse with the button pressed
 fn motion_notify_event(application: &gtk::ApplicationWindow,
     ev: &gdk::EventMotion) -> glib::signal::Inhibit {
 
-    debug!("Got motion event on coord: ({}, {})",
-        ev.get_coords().unwrap().0,
-        ev.get_coords().unwrap().1);
+    // debug!("Got motion event on coord: ({}, {})",
+        // ev.get_coords().unwrap().0,
+        // ev.get_coords().unwrap().1);
 
-    application.queue_draw();
+    let surface = cairo::ImageSurface::create(cairo::Format::ARgb32, 640, 480)
+        .expect("Couldn't create a surface!");
+
+    let cr = cairo::Context::new(&surface);
+
+    cr.set_source_rgb(255.0, 0.0, 0.0);
+    cr.set_line_width(7.0);
+    cr.set_line_cap(cairo::LineCap::Round);
+    cr.set_line_join(cairo::LineJoin::Round);
+
+    cr.move_to(ev.get_coords().unwrap().0, ev.get_coords().unwrap().1);
+    cr.line_to(ev.get_coords().unwrap().0, ev.get_coords().unwrap().1);
+    cr.stroke();
 
     gtk::Inhibit(false)
 }
 
+// Triggered when I press the button
 fn button_press_event(application: &gtk::ApplicationWindow,
     ev: &gdk::EventButton) -> glib::signal::Inhibit {
 
@@ -70,8 +82,21 @@ fn button_press_event(application: &gtk::ApplicationWindow,
         ev.get_coords().unwrap().0,
         ev.get_coords().unwrap().1);
 
-    // draw_line(ctx: &cairo::Context, x: f64, y: f64);
-    application.queue_draw();
+    let surface = cairo::ImageSurface::create(cairo::Format::ARgb32, 640, 480)
+        .expect("Couldn't create a surface!");
+
+    let cr = cairo::Context::new(&surface);
+
+    cr.set_source_rgb(255.0, 0.0, 0.0);
+    cr.set_line_width(7.0);
+    cr.set_line_cap(cairo::LineCap::Round);
+    cr.set_line_join(cairo::LineJoin::Round);
+
+    cr.move_to(ev.get_coords().unwrap().0, ev.get_coords().unwrap().1);
+    cr.line_to(0.0, 0.0);
+    cr.stroke();
+    cr.paint();
+    cr.fill();
 
     gtk::Inhibit(false)
 }
@@ -81,18 +106,15 @@ fn button_press_event(application: &gtk::ApplicationWindow,
 fn draw_line(ctx: &cairo::Context, x: f64, y: f64) -> bool {
     debug!("Drawing line from: ({}, {}) to ({}, {})", x, y, x, y);
 
-    // I wish I could do PineappleData.cr.line_to(x, y), but
-    // I can't make it because it would be a mutable global structure,
-    // so I need to create a cairo context here
-    // let ctx = cairo::Context::new();
-
     ctx.set_source_rgb(255.0, 0.0, 0.0);
-    ctx.set_line_width(7.0);
+    ctx.set_line_width(2.0);
     ctx.set_line_cap(cairo::LineCap::Round);
     ctx.set_line_join(cairo::LineJoin::Round);
 
     ctx.move_to(x, y);
     ctx.line_to(x, y);
+    ctx.paint();
+    ctx.fill();
     ctx.stroke();
 
     true
